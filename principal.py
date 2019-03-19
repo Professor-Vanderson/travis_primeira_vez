496be0cbaaafc7eabb3ff4811eaf5c10529c4e82
professor-vanderson <vanderson.bossi@hotmail.com>
2019-03-18 21:55:53 -0300

    Alterações

--- principal.py
+++ principal.py
@@ -1,12 +1,8 @@
 def somar(x,y):
-    return x + y
+    return x * y
 
 def subtrair(x,y):
     return x - y
-
-'''n1 = int(input("Informe n1: "))
-n2 = int(input("Informe n2: "))
-
-somar(n1,n2)'''
+    
 def mult():
     pass
